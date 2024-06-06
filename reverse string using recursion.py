151e8a35f66a0fd8672f3b19f1a77bbf3cce9427
NaveenNS07 <163515307+NaveenNS07@users.noreply.github.com>
2024-06-06 18:07:46 +0530

    Delete reverse string using recursion.py

--- reverse string using recursion.py
+++
@@ -1,8 +0,0 @@
-def reverse(n):
-   if len(n) == 0:
-      return n
-   else:
-      return reverse(n[1:]) + n[0]
-result = str(input("Enter the string that needs to be reversed : "))
-print("The string is :",result)
-print("The reversed string is :",reverse(result))
